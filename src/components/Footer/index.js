4207c4bbd61995ae99843e4b41feaf5c1eaab33d
henok tilahun <henokcity@yahoo.com>
2020-06-26 13:19:32 -0400

    removed jambotron form home page and linked github & linkedin

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,10 +5,10 @@ function Footer() {
         <footer>
             <nav className="navbar navbar-dark">
                 <div className="col-12 text-center">
-                    <a className="nav-link fab fa-github-square" href="https://www.github.com">
+                    <a className="nav-link fab fa-github-square" href="https://www.github.com/henokcity">
                         <span className="d-inline d-md-none">GitHub</span>
                     </a>
-                    <a className="nav-link fab fa-linkedin" href="https://www.linkedin.com">
+                    <a className="nav-link fab fa-linkedin" href="https://www.linkedin.com/in/henoktilahun">
                         <span className="d-inline d-md-none">Linkedin</span>
                     </a>
                 </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Jumbotron from "../components/Jumbotron";
 import About from "../components/About";
 
 function Home() {
     return (
         <div>
-            <Jumbotron />
             <About/>
         </div>
     );
