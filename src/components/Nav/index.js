49e239c01be4329e8420786487fd8bfd20d1f747
henok tilahun <henokcity@yahoo.com>
2020-06-26 12:07:05 -0400

    removed icons from navbar and margin added to nav btns

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -12,26 +12,6 @@ function Nav() {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <a className="nav-link" href="https://twitter.com" title="Twitter">
-              <span className="d-inline d-sm-none">Twitter</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="https://facebook.com" title="Facebook">
-              <span className="d-inline d-sm-none">Facebook</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link fab fa-github-square" href="https://www.github.com">
-              <span className="d-inline d-md-none">GitHub</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link fab fa-linkedin" href="https://www.linkedin.com">
-              <span className="d-inline d-md-none">Linkedin</span>
-            </a>
-          </li>
-          <li className="nav-item">
             <a href="projects" className="btn btn-sm">Projects</a>
           </li>
           <li className="nav-item">
